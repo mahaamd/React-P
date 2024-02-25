3c77ee8856017a8f82ea211f7ce85645847b064d
mahaamd <mohammadamiri.ma98@gmail.com>
2024-02-25 18:53:27 +0330

    initial styling

---
+++ reusable/reusablecextcomponent/reusablecomponent/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        defbuttonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+function TextExpander({
+  collapsedNumWords = 20,
+  // expandButtonText = "Show text",
+  // collapseButtonText = "Collapse text",
+  buttonColor = "#ff6622",
+  children,
+  defbuttonText = "Show text",
+  className = "",
+}) {
+  const [buttonText, setbuttonText] = useState(defbuttonText);
+  console.log(typeof children);
+  return (
+    <>
+      <div className={className}>
+        {buttonText === "Show text"
+          ? children.slice(0, collapsedNumWords)
+          : children}
+        <button
+          style={{ backgroundColor: buttonColor }}
+          onClick={() =>
+            buttonText === "Show text"
+              ? setbuttonText("Collapse text")
+              : setbuttonText("Show text")
+          }
+        >
+          {buttonText}
+        </button>
+      </div>
+      {/* <hr style={{ border: "1px solid gray" }} /> */}
+    </>
+  );
+}
